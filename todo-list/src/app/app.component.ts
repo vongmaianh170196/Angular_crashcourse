b2bb7664dc7bf88d168f672b54c889f4bdd8cfc2
Anh <vongmaianh170196@gmail.com>
2019-12-07 16:51:37 +0200

    post, update, delete

---
+++ todo-list/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // name:string = 'Anh';
+
+  // constructor(){
+  //   this.name ="Ted";
+  //   this.changeName("John")
+  // }
+
+  // changeName(name:string):void{
+  //   this.name=name
+  // }
+}
